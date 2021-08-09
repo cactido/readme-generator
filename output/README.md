f133599391708c8c7d450966e20d2aee4916fbc4
Travis Russell <mtrussell@gmail.com>
2021-08-09 17:57:44 -0600

    added terniary operators to generate table of contents

--- output/README.md
+++ output/README.md
@@ -1,2 +1,23 @@
-# test
 
+  # test
+
+  ***
+
+  ## Description
+  a test project
+
+  ***
+
+  ## Table of Contents
+  - [Installation](#installation)
+  - [Usage](#usage)
+  - [Contributing](#contributing)
+  - [Tests](#tests)
+  - [Questions](#questions)
+  - [License](#license)
+
+  
+  ## License
+  Licensed under [MIT License](https://opensource.org/licenses/MIT).
+  
+  
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,14 +1,12 @@
 //returns an MD syntax license badge if a license was selected
 function renderLicenseBadge(license) {
-  const badges = [
-    {
+  const badges = {
       'Apache License 2.0': '[![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)]',
       'ISC': ' [![License: ISC](https://img.shields.io/badge/License-ISC-blue.svg)]',
       'GNU GPL v2.0': '[![License: GPL v2](https://img.shields.io/badge/License-GPL%20v2-blue.svg)]',
       'GNU GPL v3.0': '[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)]',
       'MIT License': '[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)]'
     }
-  ]
   //returns license link if a license was requested or an empty string if not
   if (license) {
     return badges[license];
@@ -17,31 +15,47 @@ function renderLicenseBadge(license) {
 }
 //returns an MD syntax link if a license was selected
 function renderLicenseLink(license) {
-  const links = [
-    {
+  const links = {
       'Apache License 2.0': '[Apache License 2.0](https://opensource.org/licenses/Apache-2.0)',
       'ISC': '[ISC](https://opensource.org/licenses/ISC)',
       'GNU GPL v2.0': '[GNU GPL v2.0](https://www.gnu.org/licenses/old-licenses/gpl-2.0.en.html)',
       'GNU GPL v3.0': '[[GNU GPL v3.0](https://www.gnu.org/licenses/gpl-3.0)',
       'MIT License': '[MIT License](https://opensource.org/licenses/MIT)'
     }
-  ]
-
+  
   if (license) {
     return links[license];
   }
   return '';
 }
-
-// TODO: Create a function that returns the license section of README
-// If there is no license, return an empty string
-function renderLicenseSection(license) { }
-
-// TODO: Create a function to generate markdown for README
+//returns the rendered license section
+function renderLicenseSection(license) {
+  console.log(license);
+  return `
+  ## License
+  Licensed under ${renderLicenseLink(license)}.
+  `
+}
+//generates the entire readme from the constituent sections
 function generateMarkdown(data) {
-  return `# ${data.title}
+  return `
+  # ${data.title}
 
-`;
+  ## Description
+  ${data.description}
+
+  ***
+
+  ## Table of Contents
+  ${data.installation ?  `- [Installation](#installation)` : ``}
+  ${data.usage ? `- [Usage](#usage)`: ``}
+  ${data.contributing ? `- [Contributing](#contributing)`: ``}
+  ${data.license ? `- [Tests](#tests)`: ``}
+  ${data.license ? `- [Questions](#questions)`: ``}
+  ${data.license ? `- [License](#license)`: ``}
+
+  ${renderLicenseSection(data.license)}
+  `;
 }
 
 module.exports = generateMarkdown;
